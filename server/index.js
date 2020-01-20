f285b1fc3071cbf8396bf56808a6d3b4a02a39f5
bunnygarg11 <mohitgarg083@gmail.com>
2020-01-20 23:37:13 +0530

    44

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
-require('./services/passport');
+const mongoose=require("mongoose")
+require("./models/User")
 
+require('./services/passport');
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/survey",{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+}).then(()=>{
+    console.log("Mongodb connected")
+}).catch((err)=>{
+    console.log(err)
+})
 
 require('./routes/authRoutes')(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,18 +1,36 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+// const keys = require('../config/keys');
+const mongoose = require("mongoose")
+const User = mongoose.model("users")
+
+passport.serializeUser((user,done)=>{
+  done(null,user.id)
+})
+
+passport.deserializeUser((id,done)=>{
+  User.findById(id).then(user=>{
+    done(null,user)
+  })
+})
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID:"622589427817-8idgba1ks9pv3p8o54a3ug1hmp8mb613.apps.googleusercontent.com",
+      clientID: "622589427817-8idgba1ks9pv3p8o54a3ug1hmp8mb613.apps.googleusercontent.com",
       clientSecret: "AO0DSafmlmLeQpG7ZIBhYebk",
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser)
+        }
+        else {
+          new User({ googleId: profile.id }).save().then(user => done(null, user))
+        }
+
+      })
     }
   )
 );
